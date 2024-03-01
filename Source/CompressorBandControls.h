b6e098549eb6222d85c95576549bf21903f245f7
LucasVerdelho <pg50221@alunos.uminho.pt>
2024-03-01 00:35:39 +0000

    CompressorBandControlls Refactored

---
+++ Source/CompressorBandControls.h
@@ -0,0 +1,45 @@
+/*
+  ==============================================================================
+
+    CompressorBandControls.h
+    Created: 1 Mar 2024 12:33:07am
+    Author:  despacito
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+
+struct CompressorBandControls : juce::Component
+{
+    CompressorBandControls(juce::AudioProcessorValueTreeState& apvts);
+    void resized() override;
+    void paint(juce::Graphics& g) override;
+
+private:
+    juce::AudioProcessorValueTreeState& apvts;
+
+    RotarySliderWithLabels attackSlider, releaseSlider, thresholdSlider;
+    RatioSlider ratioSlider;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> attackSliderAttachment,
+        releaseSliderAttachment,
+        thresholdSliderAttachment,
+        ratioSliderAttachment;
+
+    juce::ToggleButton bypassButton, soloButton, muteButton, lowBand, midBand, highBand;
+
+    using BtnAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
+    std::unique_ptr<BtnAttachment> bypassButtonAttachment,
+        soloButtonAttachment,
+        muteButtonAttachment;
+
+    juce::Component::SafePointer<CompressorBandControls> safePtr{ this };
+
+    void updateAttachments();
+
+};
